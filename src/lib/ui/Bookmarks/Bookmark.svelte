aff76b85247919cee4c546c3373a61fb00bd81e3
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-01-03 18:02:38 -0500

    [feat] move edit icon to parent

--- src/lib/ui/Bookmarks/Bookmark.svelte
+++ src/lib/ui/Bookmarks/Bookmark.svelte
@@ -1,68 +1,20 @@
 <script lang="ts">
-	// import type { Bookmark } from '../data/types';
+	import type { Bookmark } from '$lib/data/types'
 
 	import { settings } from '$lib/data/settings/settingsStore'
 	import { scale, fade } from 'svelte/transition'
-	import type { Bookmark } from '$lib/data/types'
-	import { createEventDispatcher } from 'svelte'
-	import Edit from '$lib/icons/Edit.svelte'
+
 	import BookmarkArt from './BookmarkArt.svelte'
 
 	export let i: number
 	export let hovering: number
 	export let dragging = false
 	export let bookmark: Bookmark
-	const { url, tags, title, image, background, foreground, description } = bookmark
-
-	const dispatch = createEventDispatcher()
-
-	let timer: NodeJS.Timeout,
-		showEditIcon = false,
-		expandEditIcon = false
-
-	function smoothOver(fn: Function, delay = 500) {
-		if (dragging) return
-		timer && clearTimeout(timer)
-		timer = setTimeout(() => {
-			fn()
-		}, delay)
-	}
-
-	function smoothOut(fn: Function, delay = 300) {
-		timer && clearTimeout(timer)
-		timer = setTimeout(() => {
-			fn()
-		}, delay)
-	}
-
-	function handleClick(e: MouseEvent) {
-		if (dragging) e.preventDefault()
-	}
+	const { url, title, image, background, foreground } = bookmark
 </script>
 
-<div
-	class="bookmark-container"
-	on:mouseover={() => smoothOver(() => (showEditIcon = true))}
-	on:focus={() => (showEditIcon = true)}
-	on:mouseout={() => smoothOut(() => (showEditIcon = false))}
-	on:blur={() => (showEditIcon = false)}
->
-	{#if showEditIcon && !dragging}
-		<div
-			on:mouseover={() => smoothOver(() => (expandEditIcon = true))}
-			on:focus={() => (expandEditIcon = true)}
-			on:mouseout={() => smoothOut(() => (expandEditIcon = false))}
-			on:blur={() => (expandEditIcon = false)}
-			class="edit"
-			class:expand={expandEditIcon}
-			transition:scale={{ duration: 150 }}
-			on:click|preventDefault={() => dispatch('showEditor', { bookmark, index: i })}
-		>
-			<Edit />
-		</div>
-	{/if}
-
-	<a target="_blank" href={url} draggable="false" on:click|preventDefault={handleClick} class:dragging>
+<div class="bookmark-container">
+	<a target="_blank" href={url} draggable="false" class:dragging style="pointer-events: none;">
 		{#if image}
 			<div
 				transition:scale={{ duration: 200 + 50 * i }}
@@ -83,7 +35,9 @@
 					alt={title}
 				/>
 				{#if (title && $settings.showTitle) || hovering == i}
-					<p transition:fade={{ duration: 100 }}>{title}</p>
+					{#if !dragging}
+						<p transition:fade={{ duration: 100 }}>{title}</p>
+					{/if}
 				{/if}
 			</div>
 		{:else}
@@ -108,8 +62,8 @@
 		flex-direction: column;
 		justify-content: flex-end;
 
-		height: 100%;
-		width: 100%;
+		height: var(--size, 100%);
+		width: var(--size, 100%);
 	}
 	.bookmark {
 		display: flex;
@@ -118,8 +72,8 @@
 		flex-direction: column;
 		justify-content: flex-end;
 
-		width: 100%;
-		height: 100%;
+		width: var(--size, 100%);
+		height: var(--size, 100%);
 		margin: auto;
 
 		border-radius: 10px;
@@ -168,28 +122,5 @@
 	a.dragging {
 		pointer-events: none;
 		cursor: inherit;
-	}
-
-	.edit {
-		position: absolute;
-		top: -10px;
-		right: -10px;
-
-		width: max-content;
-		width: 2rem;
-		height: max-content;
-
-		border-radius: 5px;
-
-		font-size: 2rem;
-		line-height: 0.2rem;
-
-		cursor: pointer;
-		transition: 0.2s;
-		transform: scale(1);
-		z-index: 5;
-	}
-	.edit:hover {
-		transform: scale(1.1);
 	}
 </style>
