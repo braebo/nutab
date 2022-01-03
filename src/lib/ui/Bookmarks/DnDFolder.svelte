be1933ff3447e6220e2dcd52e5f66dc7255eeb99
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-01-03 18:04:07 -0500

    [feat] new draggable grid

--- src/lib/ui/Bookmarks/DnDFolder.svelte
+++ src/lib/ui/Bookmarks/DnDFolder.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
+	import { scale } from 'svelte/transition'
 	// Data
 	import { showGuidelines } from '$lib/data/settings/settingsStore'
+	import { smoothOut, smoothOver } from '$lib/utils/smoothHover'
 	import { grid, gridDimensions } from '$lib/stores/gridStore'
 	import { activeFolder } from '$lib/data/dbStore'
 	import { initDB } from '$lib/data/transactions'
 
 	// Components
 	import Tooltip from '$lib/ui/Tooltip.svelte'
+	import Edit from '$lib/icons/Edit.svelte'
 	import Bookmark from './Bookmark.svelte'
 
 	// Utils
@@ -16,27 +19,212 @@
 	let hovering: number | null = null
 
 	onMount(() => initDB())
+
+	let dragging = false
+	// The element to drag
+	let active: number = null
+	// The element to swap with
+	let target: number = null
+	let lastTarget: number = null
+	// Mouse movement for dragging active element
+	let move = { x: 0, y: 0 }
+	// Temporary positions for animations while dragging
+	let positionProxy = Array($gridDimensions.positions.length).fill(null)
+	// Cell transition animation duration in ms
+	const transitionDuration = 250
+	// Temporarily disables target detection
+	let cooldown = false
+	// Temporarily disables transitions
+	let disableTransitions = false
+	// A ref to each cell
+	let cells: HTMLElement[] = []
+
+	let showEditIcon: boolean[] = []
+	let expandEditIcon: boolean[] = []
+
+	const handleMouseUp = (e: MouseEvent) => {
+		// If we have a target, swap the elements
+		if (target !== null && active != null && target != active) {
+			swap(active, target)
+		}
+		// Reset all the things
+		dragging = false
+		active = null
+		target = null
+		lastTarget = null
+		move = { x: 0, y: 0 }
+		positionProxy = positionProxy.fill(null)
+	}
+
+	const handleMouseDown = (e: MouseEvent) => {
+		// Store index of active item located in its classname. i.e. - "item-3"
+		const activeClass = (e.target as Element).classList[0]
+		if (activeClass?.startsWith('item-')) {
+			active = parseInt(activeClass.split('-')[1])
+		}
+	}
+
+	const handleMouseMove = (e: MouseEvent) => {
+		// Make sure we have an active cell first
+		if (active === null) return
+		else dragging = true
+
+		// Update movement vector
+		move.x += e.movementX
+		move.y += e.movementY
+
+		// If we're hovering over a cell and not on cooldown
+		if (!cooldown && (e.target as Element).classList[0]?.split('-')[1] != null) {
+			// Store the target cell's index
+			target = parseInt((e.target as Element).classList[0]?.split('-')[1])
+
+			// If our target cell has changed
+			if (lastTarget != null && lastTarget != target) {
+				// go back to original position
+				positionProxy[lastTarget] = null
+			}
+
+			// If the target cell isn't the current active cell
+			if (target != active) {
+				// Move it to our active's cell position
+				positionProxy[target] = $gridDimensions.positions[active]
+			} else {
+				// We have returned to the original cell, so
+				// we can reset all positions
+				positionProxy = positionProxy.fill(null)
+			}
+
+			// Update lastTarget
+			lastTarget = target
+
+			// apply cooldown to avoid jank
+			setCooldown()
+		}
+	}
+
+	const handleItemMouseOver = (i: number) => {
+		hovering = i
+		smoothOver(() => (showEditIcon[i] = true), 1000)
+	}
+	const handleItemMouseOut = (i: number) => {
+		// hovering = null
+		// smoothOver(() => (showEditIcon[i] = false))
+	}
+
+	$: if (dragging) {
+		hovering = null
+		showEditIcon.fill(false)
+	}
+
+	let cooldownTimer: NodeJS.Timeout
+	function setCooldown() {
+		cooldown = true
+		clearTimeout(cooldownTimer)
+		cooldownTimer = setTimeout(() => {
+			cooldown = false
+		}, transitionDuration * 0.75)
+	}
+
+	$: getCellPosition = (i: number) => {
+		if (positionProxy[i] != null) {
+			return `translate(${positionProxy[i].x}px, ${positionProxy[i].y}px)`
+		} else {
+			return `translate(${$gridDimensions.positions[i].x}px, ${$gridDimensions.positions[i].y}px)`
+		}
+	}
+
+	$: $gridDimensions
+
+	let swapTimer: NodeJS.Timeout
+	const swap = (a: number, b: number) => {
+		disableTransitions = true
+
+		const _a = $activeFolder.bookmarks[a]
+		$activeFolder.bookmarks[a] = $activeFolder.bookmarks[b]
+		$activeFolder.bookmarks[b] = _a
+
+		clearTimeout(swapTimer)
+		swapTimer = setTimeout(async () => {
+			disableTransitions = false
+		}, 0)
+	}
 </script>
 
-<div class="grid" style="width: {$grid.gridWidth}px;" class:showGuidelines={$showGuidelines}>
-	{#if $activeFolder?.bookmarks}
-		{#each $activeFolder.bookmarks as bookmark, i}
-			<div
-				class="grid-item"
-				style="
-					width: {$grid.iconSize}px;
-					height: {$grid.iconSize}px;
-					margin: {$grid.gridGap}px;
-				"
-				on:mouseover={() => (hovering = i)}
-				on:mouseout={() => (hovering = null)}
-				on:focus={() => (hovering = i)}
-				on:blur={() => (hovering = null)}
-			>
-				<Bookmark {bookmark} {hovering} {i} on:showEditor />
-			</div>
-		{/each}
-	{/if}
+<svelte:window on:mousedown={handleMouseDown} on:mouseup={handleMouseUp} on:mousemove={handleMouseMove} />
+
+<div
+	class="grid"
+	class:showGuidelines={$showGuidelines}
+	class:dragging
+	style="
+		width: {$grid.gridWidth}px;
+		height: {$gridDimensions.gridHeight}px;
+		--item-size: {$grid.iconSize}px;
+	"
+>
+	{#key [$activeFolder?.bookmarks, $gridDimensions]}
+		{#if $grid.items}
+			{#each $grid.items as bookmark, i}
+				<div
+					class="cell-{i} cell"
+					class:active={i == active}
+					class:target={target === i}
+					style="
+						/* stylelint-disable */
+						width: {$gridDimensions.totalItemSize}px;
+						height: {$gridDimensions.totalItemSize}px;
+						transform: {getCellPosition(i)};
+						transition: {disableTransitions ? 'none' : `${transitionDuration}ms`};
+					"
+					bind:this={cells[i]}
+				>
+					<div
+						class="item-{i} grid-item"
+						class:active={active === i}
+						class:target={target === i}
+						draggable="false"
+						class:dragging
+						class:disableTransitions
+						style="transform: translate({active === i ? `${move.x}px, ${move.y}px` : `0, 0`});"
+						on:mouseover={() => handleItemMouseOver(i)}
+						on:mouseout={() => handleItemMouseOut(i)}
+						on:focus={() => handleItemMouseOver(i)}
+						on:blur={() => handleItemMouseOut(i)}
+					>
+						<div class="grid-image">
+							<Bookmark
+								{bookmark}
+								{hovering}
+								{dragging}
+								{i}
+								on:showEditor
+								--size={$grid.iconSize + 'px'}
+							/>
+						</div>
+						{#if showEditIcon[i] && !dragging}
+							<div
+								on:mouseover={() => handleItemMouseOver(i)}
+								on:mouseout={() => handleItemMouseOut(i)}
+								on:focus={() => handleItemMouseOver(i)}
+								on:blur={() => handleItemMouseOut(i)}
+								class="edit"
+								class:expand={expandEditIcon[i]}
+								transition:scale={{ duration: 150 }}
+								on:click|preventDefault={() => dispatch('showEditor', { bookmark, index: i })}
+							>
+								<Edit />
+							</div>
+						{/if}
+						<!-- <div
+							class="grid-image"
+							style="background-image: url({bookmark?.image});width:var(--item-size);height:var(--item-size);"
+							draggable="false"
+						/> -->
+					</div>
+				</div>
+			{/each}
+		{/if}
+	{/key}
 	<div class="add-bookmark" on:click={() => dispatch('newBookmark')}>
 		<Tooltip content="New_Bookmark" placement="bottom" offset={[0, 10]}>+</Tooltip>
 	</div>
@@ -45,14 +233,9 @@
 <style lang="scss">
 	.grid {
 		position: relative;
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: center;
 
-		width: max-content;
-		max-width: 80vw;
-		margin: 0 auto;
-		margin-top: 5%;
+		margin: 1vh auto;
+		box-sizing: content-box;
 
 		// for $showGuidelines
 		border: 1px solid #0000;
@@ -60,11 +243,55 @@
 		border-bottom: none;
 
 		transition: border 0.2s ease-in-out;
+
+		&.showGuidelines {
+			border: 1px solid rgba(var(--dark-d-rgb), 0.5);
+			border-top: none;
+			border-bottom: none;
+		}
+
+		z-index: 1;
+		user-select: none;
 	}
-	.showGuidelines {
-		border: 1px solid rgba(var(--dark-d-rgb), 0.5);
-		border-top: none;
-		border-bottom: none;
+	.cell {
+		position: absolute;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		&.active {
+			z-index: 10 !important;
+		}
+	}
+	.grid-item {
+		width: var(--item-size);
+		height: var(--item-size);
+		margin: auto;
+
+		color: white;
+
+		cursor: pointer;
+		pointer-events: all;
+
+		transition: 0.2s ease-in-out;
+
+		&.active {
+			pointer-events: none;
+			z-index: 10 !important;
+
+			transition: none;
+		}
+
+		&.target:not(.active) {
+			z-index: -1;
+		}
+
+		&.disableTransitions {
+			transition: none;
+		}
+	}
+	.dragging {
+		cursor: grabbing;
 	}
 
 	.add-bookmark {
@@ -89,5 +316,36 @@
 
 			transition: opacity 0.4s;
 		}
+	}
+	.grid-image {
+		width: 100%;
+		height: 100%;
+		background-size: contain;
+		background-repeat: no-repeat;
+		background-position: center;
+
+		pointer-events: none;
+	}
+	.edit {
+		position: absolute;
+		top: -10px;
+		right: -10px;
+
+		width: max-content;
+		width: 2rem;
+		height: max-content;
+
+		border-radius: 5px;
+
+		font-size: 2rem;
+		line-height: 0.2rem;
+
+		cursor: pointer;
+		transition: 0.2s;
+		transform: scale(1);
+		z-index: 15;
+	}
+	.edit:hover {
+		transform: scale(1.1);
 	}
 </style>
