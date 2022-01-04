f39a95698e9e24cf406d540b6fddc51b9d7ef69e
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-01-03 20:41:06 -0500

    [fix] keep both timers cleared

--- src/lib/utils/smoothHover.ts
+++ src/lib/utils/smoothHover.ts
@@ -1,16 +1,19 @@
 let overTimer: NodeJS.Timeout
 export function smoothOver(fn: Function, delay = 500, bypass?: boolean) {
 	if (bypass) return
-	clearTimeout(overTimer)
+	clearTimers()
 	overTimer = setTimeout(() => {
+		console.log(fn())
 		fn()
 	}, delay)
 }
 
 let outTimer: NodeJS.Timeout
 export function smoothOut(fn: Function, delay = 300) {
-	clearTimeout(outTimer)
+	clearTimers()
 	outTimer = setTimeout(() => {
 		fn()
 	}, delay)
 }
+
+const clearTimers = () => [outTimer, overTimer].forEach((t) => clearTimeout(t))
