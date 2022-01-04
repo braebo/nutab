563cd8cadcc76276e86d4b036ab31cd961529fde
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-01-03 23:26:52 -0500

    [feat] add action type

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,27 @@
 /// <reference types="@sveltejs/kit" />
+
+/**
+ * https://github.com/paolotiu/svelte-useactions/blob/main/src/index.ts
+ * *Action types
+ */
+
+/**
+ * The return type of an action.
+ */
+export type ActionReturn<Params> = {
+	destroy?: () => void
+	update?: (params: Params) => void
+} | void
+
+/**
+ * Action type shim
+ */
+export type ActionLike<Node extends HTMLElement> = (node: Node, params: any) => any
+
+/**
+ * A primitive Action
+ */
+export type Action<Node extends HTMLElement = HTMLElement, Params extends any = undefined> = (
+	node: Node,
+	params?: Params
+) => ActionReturn<Params>
