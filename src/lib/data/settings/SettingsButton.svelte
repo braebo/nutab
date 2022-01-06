1b675039ee6014ccd6a32f66aca17ff3022304a9
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-01-06 00:11:21 -0500

    [fix] settings button not closing

--- src/lib/data/settings/SettingsButton.svelte
+++ src/lib/data/settings/SettingsButton.svelte
@@ -2,29 +2,36 @@
 	import { showSettings } from './settingsStore'
 </script>
 
-<div class="settings-button" on:click={() => ($showSettings = !$showSettings)}>
+<div id="settings-button" on:click={() => ($showSettings = !$showSettings)} class:active={$showSettings}>
 	<div class="dot" />
 	<div class="dot" />
 	<div class="dot" />
 </div>
 
 <style>
-	.settings-button {
+	#settings-button {
 		position: absolute;
 		top: 0;
-		right: 3px;
+		right: 0;
 		display: flex;
 		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 4px;
 
-		justify-content: space-around;
-
-		/* width: 50px; */
-		height: 20px;
-		margin: 15px;
+		height: 50px;
+		width: 40px;
+		box-sizing: content-box;
 
 		font-size: 25px;
 
 		cursor: pointer;
+
+		transition: 150ms;
+	}
+	.active {
+		top: 3px !important;
+		gap: 10px !important;
 	}
 
 	.dot {
@@ -37,5 +44,7 @@
 		border-radius: 100%;
 
 		background: var(--dark-c);
+
+		pointer-events: none;
 	}
 </style>
--- src/lib/utils/clickOutside.ts
+++ src/lib/utils/clickOutside.ts
@@ -1,20 +1,21 @@
-export function clickOutside(node) {
-	const handleClick = (event) => {
+export function clickOutside(node: Element) {
+	const handleClick = (event: MouseEvent) => {
+		console.log(event.target)
 		if (
 			node &&
-			!node.contains(event.target) &&
+			!node.contains(event.target as Element) &&
 			!event.defaultPrevented &&
-			event.target.id != 'hamburger'
+			(event.target as Element).id != 'settings-button'
 		) {
-			node.dispatchEvent(new CustomEvent('click_outside', node));
+			node.dispatchEvent(new CustomEvent('click_outside', node))
 		}
-	};
+	}
 
-	document.addEventListener('click', handleClick, true);
+	document.addEventListener('click', handleClick, true)
 
 	return {
 		destroy() {
-			document.removeEventListener('click', handleClick, true);
-		},
-	};
+			document.removeEventListener('click', handleClick, true)
+		}
+	}
 }
