07afec4f9683b437822d48f29da5fb344779e714
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-01-12 21:30:19 -0500

    [feat] better id parsing

--- src/lib/utils/idFromClassList.ts
+++ src/lib/utils/idFromClassList.ts
@@ -1,14 +1,9 @@
 export const idFromClassList = (classes: DOMTokenList, prefix: string): number | null => {
-	let i = null
+	let i: number = null
 	classes.forEach((c) => {
-		if (c.includes(prefix) && c.length > 3) {
+		if (i === null && c.includes(prefix) && c.length > 3) {
 			i = parseInt(c.split(prefix)[1])
 		}
 	})
-	console.log(i, prefix)
-	if (i || i === 0) return i
-	else {
-		console.log('idFromClassList: No id found.')
-		return null
-	}
+	return i || i === 0 ? i : null
 }
