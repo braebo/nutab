bd7906987d4f63301f246b480ca9080349c6d7cc
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-01-16 17:48:36 -0500

    [feat] add disabled mode

--- src/lib/ui/Folders/DeleteFolder.svelte
+++ src/lib/ui/Folders/DeleteFolder.svelte
@@ -1,28 +1,14 @@
 <script lang="ts">
 	import { editorContext } from '$lib/stores/bookmarkEditor'
-	// TODO:
-	// import { deleteFolder_db } from '$lib/data/transactions'
 	import TrashIcon from '$lib/data/icons/TrashIcon.svelte'
-	import { reRender } from '$lib/stores/gridStore'
-	import { createEventDispatcher } from 'svelte'
 	import Tooltip from '$lib/ui/Tooltip.svelte'
 
-	const dispatch = createEventDispatcher()
-
-	export let folder_id: string
-
-	function _delete() {
-		// TODO:
-		// deleteFolder_db(folder_id)
-		alert('todo')
-		dispatch('close')
-		$reRender = !$reRender
-	}
+	export let disabled = true
 </script>
 
 {#if $editorContext == 'edit'}
-	<div class="delete" on:click={_delete}>
-		<Tooltip content="Delete" offset={[0, 10]} placement="top">
+	<div class="delete" on:click class:disabled>
+		<Tooltip content={disabled ? `Unable_to_delete_your_only_Folder` : 'Delete'} offset={[0, 10]} placement="top">
 			<TrashIcon />
 		</Tooltip>
 	</div>
@@ -33,5 +19,10 @@
 		position: absolute;
 		left: -6rem;
 		bottom: 4px;
+	}
+	.disabled {
+		opacity: 0.5;
+
+		cursor: not-allowed;
 	}
 </style>
