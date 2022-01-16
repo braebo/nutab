63db15949faaa39664e7489fc0f9a2f0a5f66aa5
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-01-16 17:47:06 -0500

    [feat] use master bookmarks store

--- src/lib/stores/gridStore.ts
+++ src/lib/stores/gridStore.ts
@@ -1,15 +1,15 @@
 import { settings } from '$lib/data/settings/settingsStore'
-import { activeBookmarks } from '$lib/data/dbStore'
+import { activeFolderBookmarks } from '$lib/data/dbStore'
 import { writable, derived } from 'svelte/store'
 
 // Used to trigger the grid's {#key} block
 export const reRender = writable(false)
 
-export const grid = derived([activeBookmarks, settings], ([$activeBookmarks, $settings]) => ({
+export const grid = derived([activeFolderBookmarks, settings], ([$activeFolderBookmarks, $settings]) => ({
 	gridWidth: $settings.ranges.gridWidth.value,
 	iconSize: $settings.ranges.iconSize.value,
 	gridGap: $settings.ranges.gridGap.value,
-	items: $activeBookmarks
+	items: $activeFolderBookmarks
 }))
 
 export const gridDimensions = derived(grid, ($grid) => {
