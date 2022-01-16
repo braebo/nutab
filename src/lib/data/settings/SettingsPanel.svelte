97e14c6c0c4fe3d6e8ede00f53db79e781a9e0dd
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-01-16 18:06:34 -0500

    [feat] new animated title toggle

--- src/lib/data/settings/SettingsPanel.svelte
+++ src/lib/data/settings/SettingsPanel.svelte
@@ -2,7 +2,7 @@
 	import { settings, showSettings, showGuidelines } from './settingsStore'
 	import { clickOutside } from '$lib/utils/clickOutside'
 	import Range from '$lib/ui/Range.svelte'
-	import { fly } from 'svelte/transition'
+	import { fly, fly } from 'svelte/transition'
 
 	let sgTimer: NodeJS.Timeout
 	const toggleShowGuidelines = (e: Event, setting: any) => {
@@ -37,14 +37,21 @@
 				</div>
 			{/each}
 			<label for="showTitle">
-				Show Title
-				<input type="checkbox" bind:checked={$settings.showTitle} />
+				<!-- <input type="checkbox" bind:checked={$settings.showTitle} /> -->
+				<div class="title-toggle" on:click={() => ($settings.showTitle = !$settings.showTitle)}>
+					{#key $settings.showTitle}
+						<div class="show-hide" in:fly={{ y: 30 }} out:fly={{ y: -30 }}>
+							{$settings.showTitle ? 'Hide' : 'Show'}
+						</div>
+					{/key}
+					<div class="titles">Titles</div>
+				</div>
 			</label>
 		</div>
 	</div>
 {/if}
 
-<style>
+<style lang="scss">
 	.control-panel {
 		position: absolute;
 		position: fixed;
@@ -97,15 +104,48 @@
 		margin: auto 0;
 	}
 
-	input[type='checkbox'] {
-		position: absolute;
-		top: 0;
-		right: -50%;
-		bottom: 0;
+	.title-toggle {
+		position: relative;
+		display: flex;
+		align-items: center;
+		justify-content: center;
 
-		width: 15px;
+		width: 120px;
 		height: 15px;
 		margin: auto;
+		padding: 1rem 2rem;
+
+		border-radius: 10px;
+		background: var(--light-a);
+		border: 1px solid var(--light-a);
+
+		font-family: var(--font);
+
+		cursor: pointer;
+		user-select: none;
+		overflow: hidden;
+
+		transition: 150ms;
+
+		&:hover {
+			border: 1px solid var(--light-c);
+		}
+
+		.show-hide {
+			position: absolute;
+			inset: 0;
+			left: -40%;
+
+			width: max-content;
+			height: max-content;
+			margin: auto;
+
+			white-space: nowrap;
+		}
+
+		.titles {
+			transform: translateX(50%);
+		}
 	}
 	.mousetrap {
 		position: absolute;
