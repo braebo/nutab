8558cac024262e9e56256d6a73af4a23f6ad60b9
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-01-16 17:45:16 -0500

    [feat] add all folder transactions

--- src/lib/data/transactions.ts
+++ src/lib/data/transactions.ts
@@ -1,9 +1,10 @@
-import type { Bookmark, Folder } from './types'
+import type { Bookmark, Folder, FolderListItem } from './types'
 
-import { activeBookmarks, activeFolder, activeFolderBookmarks, folders, tagFilter } from './dbStore'
+import { activeBookmarks, activeFolder, activeFolderBookmarks, folders, lastActiveFolderId, tagFilter, uniqueTags } from './dbStore'
 import { log, wait } from 'fractils'
 import { get } from 'svelte/store'
 import db from './db'
+import cuid from 'cuid'
 
 
 /**
@@ -59,7 +60,7 @@ export async function newBookmark_db(bookmark: Bookmark) {
 
 /**
  ** Creates a new folder.
- * @param  {folder} The bookmark to add.
+ * @param  {folder} The folder to add.
  */
 export async function newFolder_db(folder: Folder) {
 	log('🎬 Creating new folder: ', '#fd8', '#333', 25)
@@ -68,7 +69,7 @@ export async function newFolder_db(folder: Folder) {
 
 	//? Clear any filters
 	tagFilter.set(null)
-	
+
 	//? Add to folders table
 	await db.folders.add(folder)
 
@@ -78,8 +79,29 @@ export async function newFolder_db(folder: Folder) {
 	//? Update activeBookmarks store
 	activeBookmarks.set(await db.bookmarks.bulkGet(folder.bookmarks))
 
+	//? Update folders store
+	folders.set(await getAllFolders_db())
+
 	log('🏁 New Folder added', '#fd8', '#333', 25)
 }
+
+
+/**
+ ** Get's the id's of any bookmarks containing a tag from an array of tags
+ * @param {string[]} tags An array of tags
+ * @returns {string[]} An array of bookmark ids
+ */ 
+export const getBookmarksWithSelectedTags_db = async (tags: string[]): Promise<Bookmark["bookmark_id"][]> => {
+ 	//? Get all relevant bookmarks
+	const bookmarks = await db.bookmarks.where('tags').anyOf(tags).toArray()
+	//? We need to give them unique id's so they can be deleted without affecting other folders.
+	bookmarks.forEach(b => {b.bookmark_id = cuid()})
+	//? Add them to the bookmarks table
+	await db.bookmarks.bulkAdd(bookmarks)
+
+	return bookmarks.map(b => b.bookmark_id)
+}
+
 
 
 /**
@@ -90,6 +112,17 @@ export async function getBookmark_db(id: Bookmark['bookmark_id']) {
 	log(`🎬 Getting bookmark with id of ${id}`, '#fa8', 'dimgray', 25)
 
 	return db.bookmarks.where('bookmark_id').equals(id).first()
+}
+
+/**
+ * 
+ ** Get's all folders.
+ * @returns All folders
+ */
+export const getAllFolders_db = async (): Promise<FolderListItem[]> => {
+	log('Getting folders')
+	const allFolders: Folder[] = await db.folders.toArray()
+	return allFolders.map((f) => ({ folder_id: f.folder_id, icon: f.icon, title: f.title }))
 }
 
 
@@ -170,6 +203,41 @@ export async function deleteBookmark_db(id: Bookmark['bookmark_id']) {
 
 
 /**
+ ** Deletes a folder
+ * @param  {folder} The folder to delete.
+ */
+export async function deleteFolder_db(folder: Folder) {
+	const id = folder.folder_id
+	log(`🎬 Deleting folder with id of ${id}`, '#fa8', 'dimgray', 25)
+	
+	const before = await db.bookmarks.count()
+	//? Remove the folder's bookmarks
+	await db.bookmarks.bulkDelete(folder.bookmarks)
+	const after = await db.bookmarks.count()
+	log('🏁 Removed ' + (before - after) + ' bookmarks', '#fa8', 'dimgray', 25)
+	
+	//? Delete from folders table
+	await db.folders.delete(id)
+
+	//? Update activeFolder store if the deleted folder was active
+	if (get(activeFolder).folder_id === id) {
+		const fallbackFolder = await db.folders.orderBy('folder_id').first()
+		activeFolder.set(fallbackFolder)
+		lastActiveFolderId.set(fallbackFolder.folder_id)
+	}
+
+	//? Update folders store
+	folders.set(await getAllFolders_db())
+
+	//! I don't think I need the activeBookmarks store anymore?
+	// // ? Update bookmarks store
+	// activeBookmarks.set(bookmarks)
+
+	log('🏁 Folder deleted', '#fa8', 'dimgray', 25)
+}
+
+
+/**
  ** Updates a bookmark.
  * @param  {bookmark} The bookmark to update.
  */
@@ -181,4 +249,23 @@ export async function updateBookmark_db(bookmark: Bookmark) {
 	activeBookmarks.update(v => v.map(b => b.bookmark_id === bookmark.bookmark_id ? bookmark : b))
 
 	log('🏁 Bookmark updated', '#fa8', 'dimgray', 25)
+}
+
+
+/**
+ ** Updates a folder.
+ * @param  {folder} The folder to update.
+ */
+export async function updateFolder_db(folder: Folder) {
+	log('🎬 Updating folder: ', '#fa8', 'dimgray', 25)
+	log(folder)
+
+	await db.folders.put(folder)
+	// Update activeFolder store if it's the same as the one being updated
+	if (get(activeFolder).folder_id == folder.folder_id) activeFolder.set(folder)
+
+	//? Update folders store
+	folders.set(await getAllFolders_db())
+
+	log('🏁 Folder updated', '#fa8', 'dimgray', 25)
 }
