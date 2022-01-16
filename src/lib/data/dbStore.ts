a6dc740c916cfeacbe4f28be4db33d4aa8512376
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-01-16 17:45:05 -0500

    [feat] new master derived store

--- src/lib/data/dbStore.ts
+++ src/lib/data/dbStore.ts
@@ -1,28 +1,63 @@
-import type { Folder, Bookmark } from './types'
+import type { Folder, Bookmark, FolderListItem } from './types'
+import type { Writable } from 'svelte/store'
 
+import { localStorageStore, log } from 'fractils'
 import { writable, derived } from 'svelte/store'
-import { localStorageStore } from 'fractils'
 import { liveQuery } from 'dexie'
 import db from '$lib/data/db'
 
-export const folders = writable<Folder[]>()
+/**
+ ** A list of all folders (without their bookmarks) for the FolderSidebar list.
+ */
+export const folders = writable<FolderListItem[]>()
 export const activeFolder = writable<Folder>()
-
-//? Will always contain the current Folder's bookmarks
-export const activeFolderBookmarks = derived(activeFolder, ($activeFolder) =>
-	db.bookmarks.bulkGet($activeFolder?.bookmarks)
-)
 
 //? Seperate store that be manually set.  i.e. - to a tag-filtered array of bookmarks
 export const activeBookmarks = writable<Bookmark[]>()
 
 export const lastActiveFolderId = localStorageStore('lastActiveFolderId', '')
 
-export const uniqueTags: any = liveQuery(async () => await db.bookmarks.orderBy('tags').uniqueKeys())
+export const uniqueTags = liveQuery(async () => await db.bookmarks.orderBy('tags').uniqueKeys())
 
 //? Filter bookmarks by tag
 export const tagFilter = writable<string | null>(null)
-export const filteredBookmarks = derived(tagFilter, async ($tagFilter) => {
+export const filteredBookmarks = derived(tagFilter, ($tagFilter, set) => {
+	log('tagFilter', $tagFilter)
 	if ($tagFilter === null) return null
-	return await db.bookmarks.where('tags').equals($tagFilter).toArray()
+	db.bookmarks.where('tags').equals($tagFilter).toArray().then(set)
+	return () => {
+		set = () => {}
+	}
 })
+
+// Async Derived 😎 - https://github.com/sveltejs/svelte/issues/2118#issuecomment-492537687
+//? If there is a tagFilter it's applied, else this is the active folder's bookmarks
+export const activeFolderBookmarks = derived<Writable<Folder | string>[], Bookmark[]>(
+	[activeFolder, tagFilter],
+	([$activeFolder, $tagFilter], set) => {
+		if ($tagFilter === null) {
+			db.bookmarks.bulkGet(($activeFolder as Folder)?.bookmarks).then(set)
+		} else {
+			db.bookmarks
+				.where('tags')
+				.equals($tagFilter as string)
+				.toArray()
+				.then((bookmarks) => {
+					//? Some folders have similar bookmarks with unique id's, so we
+					//? need to filter out the duplicates.
+					const uniqueTitles = new Set()
+					const uniqueBookmarks = bookmarks.reduce((acc, curr) => {
+						if (!uniqueTitles.has(curr.title)) {
+							uniqueTitles.add(curr.title)
+							return [...acc, curr]
+						}
+						return acc
+					}, [])
+					set(uniqueBookmarks)
+				})
+		}
+		return () => {
+			set = () => {}
+		}
+	}
+)
