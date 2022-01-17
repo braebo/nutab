7ea2fb16a66c40946e20595485c3d3b4d063b223
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-01-17 17:53:58 -0500

    [feat] use external btn class

--- src/lib/data/settings/ShowTitle.svelte
+++ src/lib/data/settings/ShowTitle.svelte
@@ -5,7 +5,7 @@
 
 <label for="showTitle">
 	<!-- <input type="checkbox" bind:checked={$settings.showTitle} /> -->
-	<div class="title-toggle" on:click={() => ($settings.showTitle = !$settings.showTitle)}>
+	<div class="btn title-toggle" on:click={() => ($settings.showTitle = !$settings.showTitle)}>
 		{#key $settings.showTitle}
 			<div class="show-hide" in:fly={{ y: 30 }} out:fly={{ y: -30 }}>
 				{$settings.showTitle ? 'Hide' : 'Show'}
@@ -17,23 +17,8 @@
 
 <style lang="scss">
 	.title-toggle {
-		position: relative;
-		display: flex;
-		align-items: center;
-		justify-content: center;
+		width: 110px;
 
-		width: 120px;
-		height: 15px;
-		margin: auto;
-		padding: 1rem 2rem;
-
-		border-radius: 10px;
-		background: var(--light-a);
-		border: 1px solid var(--light-a);
-
-		font-family: var(--font);
-
-		cursor: pointer;
 		user-select: none;
 		overflow: hidden;
 
@@ -46,7 +31,7 @@
 		.show-hide {
 			position: absolute;
 			inset: 0;
-			left: -40%;
+			left: -42%;
 
 			width: max-content;
 			height: max-content;
@@ -56,7 +41,7 @@
 		}
 
 		.titles {
-			transform: translateX(50%);
+			transform: translateX(55%);
 		}
 	}
 </style>
