cb46bc8340461f59bbb6f28c3df97940c9937af5
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-01-17 17:53:26 -0500

    [feat] randomizebg animation

--- src/lib/data/settings/RandomizeBackground.svelte
+++ src/lib/data/settings/RandomizeBackground.svelte
@@ -1,9 +1,33 @@
-<script>
+<script lang="ts">
 	import { randomBackground } from './randomBackground'
 
+	let timer: NodeJS.Timeout
+	let animate = false
 	const handleClick = () => {
 		document.getElementById('svelte').style = randomBackground()
+		clearTimeout(timer)
+		animate = true
+		timer = setTimeout(() => {
+			animate = false
+		}, 1000)
 	}
 </script>
 
-<div class="randomize" on:click={() => handleClick()}>New Background</div>
+{#key animate}
+	<div class:animate class="btn randomize" on:click={() => handleClick()}>Shuffle Background</div>
+{/key}
+
+<style>
+	.animate {
+		animation: colorize 1s infinite;
+	}
+	@keyframes colorize {
+		from {
+			background: #ff1;
+			filter: hue-rotate(0deg);
+		}
+		to {
+			filter: hue-rotate(360deg);
+		}
+	}
+</style>
