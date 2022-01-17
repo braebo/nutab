1aece5f08162afafa318415a67a2f1799f3dde5d
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-01-16 22:15:06 -0500

    [feat] extract ShowTitle component

--- src/lib/data/settings/SettingsPanel.svelte
+++ src/lib/data/settings/SettingsPanel.svelte
@@ -2,7 +2,9 @@
 	import { settings, showSettings, showGuidelines } from './settingsStore'
 	import { clickOutside } from '$lib/utils/clickOutside'
 	import Range from '$lib/ui/Range.svelte'
-	import { fly, fly } from 'svelte/transition'
+	import { fly } from 'svelte/transition'
+	import ShowTitle from './ShowTitle.svelte'
+	import RandomizeBackground from './RandomizeBackground.svelte'
 
 	let sgTimer: NodeJS.Timeout
 	const toggleShowGuidelines = (e: Event, setting: any) => {
@@ -36,17 +38,8 @@
 					/>
 				</div>
 			{/each}
-			<label for="showTitle">
-				<!-- <input type="checkbox" bind:checked={$settings.showTitle} /> -->
-				<div class="title-toggle" on:click={() => ($settings.showTitle = !$settings.showTitle)}>
-					{#key $settings.showTitle}
-						<div class="show-hide" in:fly={{ y: 30 }} out:fly={{ y: -30 }}>
-							{$settings.showTitle ? 'Hide' : 'Show'}
-						</div>
-					{/key}
-					<div class="titles">Titles</div>
-				</div>
-			</label>
+			<ShowTitle />
+			<RandomizeBackground />
 		</div>
 	</div>
 {/if}
@@ -104,49 +97,6 @@
 		margin: auto 0;
 	}
 
-	.title-toggle {
-		position: relative;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-
-		width: 120px;
-		height: 15px;
-		margin: auto;
-		padding: 1rem 2rem;
-
-		border-radius: 10px;
-		background: var(--light-a);
-		border: 1px solid var(--light-a);
-
-		font-family: var(--font);
-
-		cursor: pointer;
-		user-select: none;
-		overflow: hidden;
-
-		transition: 150ms;
-
-		&:hover {
-			border: 1px solid var(--light-c);
-		}
-
-		.show-hide {
-			position: absolute;
-			inset: 0;
-			left: -40%;
-
-			width: max-content;
-			height: max-content;
-			margin: auto;
-
-			white-space: nowrap;
-		}
-
-		.titles {
-			transform: translateX(50%);
-		}
-	}
 	.mousetrap {
 		position: absolute;
 		right: 0;
---
+++ src/lib/data/settings/ShowTitle.svelte
@@ -0,0 +1,62 @@
+<script>
+	import { settings } from '$lib/data/settings/settingsStore'
+	import { fly } from 'svelte/transition'
+</script>
+
+<label for="showTitle">
+	<!-- <input type="checkbox" bind:checked={$settings.showTitle} /> -->
+	<div class="title-toggle" on:click={() => ($settings.showTitle = !$settings.showTitle)}>
+		{#key $settings.showTitle}
+			<div class="show-hide" in:fly={{ y: 30 }} out:fly={{ y: -30 }}>
+				{$settings.showTitle ? 'Hide' : 'Show'}
+			</div>
+		{/key}
+		<div class="titles">Titles</div>
+	</div>
+</label>
+
+<style lang="scss">
+	.title-toggle {
+		position: relative;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		width: 120px;
+		height: 15px;
+		margin: auto;
+		padding: 1rem 2rem;
+
+		border-radius: 10px;
+		background: var(--light-a);
+		border: 1px solid var(--light-a);
+
+		font-family: var(--font);
+
+		cursor: pointer;
+		user-select: none;
+		overflow: hidden;
+
+		transition: 150ms;
+
+		&:hover {
+			border: 1px solid var(--light-c);
+		}
+
+		.show-hide {
+			position: absolute;
+			inset: 0;
+			left: -40%;
+
+			width: max-content;
+			height: max-content;
+			margin: auto;
+
+			white-space: nowrap;
+		}
+
+		.titles {
+			transform: translateX(50%);
+		}
+	}
+</style>
