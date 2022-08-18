a5364cc4beafeed86fd4e4efed6655a922bd4c2d
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-08-18 16:42:21 -0400

    [feat] improved Range

--- src/lib/ui/Range.svelte
+++ src/lib/ui/Range.svelte
@@ -1,32 +1,64 @@
 <script lang="ts">
+	import { createEventDispatcher } from 'svelte'
 	import { mapRange } from 'fractils'
 	import { onDestroy } from 'svelte'
+
+	const dispatch = createEventDispatcher()
 
 	export let setting: any, name: string, range
 	const { min, max } = range as { min: number; max: number }
 
 	let el: HTMLElement
+	let track: HTMLElement
+	let thumb: HTMLElement
 	let dragging = false
 	const thumbWidth = 12
 	$: clientWidth = el?.clientWidth ?? 100
 	$: progress = mapRange(setting, min, max, 1, clientWidth - thumbWidth)
 
+	const setFromMouseX = (e: MouseEvent) => {
+		if (!el) return
+		const { clientX } = e
+		const { clientWidth } = el
+		const { left } = el.getBoundingClientRect()
+		const relativeX = clientX - left
+		// const normalized = (clientX - offset) / width
+		const normalizedProgress = mapRange(relativeX, 0, clientWidth, 0, 100)
+		setting = mapRange(normalizedProgress, 0, 100, min, max)
+		mouseDown()
+	}
+
 	const mouseUp = () => {
 		dragging = false
+
 		window.addEventListener('mousemove', mouseMove)
+
+		el.style.cursor = 'pointer'
+		track.style.cursor = 'pointer'
+		thumb.style.cursor = 'grab'
 	}
 
 	const mouseDown = () => {
 		dragging = true
+
 		window.addEventListener('mouseup', mouseUp, { once: true })
 		window.addEventListener('mousemove', mouseMove)
+
+		el.style.cursor = 'grabbing'
+		el.parentElement.style.cursor = 'grabbing'
+		track.style.cursor = 'grabbing'
+		thumb.style.cursor = 'grabbing'
 	}
 
 	const mouseMove = (e: MouseEvent) => {
 		if (!dragging || !el) return
+
 		setting += e.movementX * ((1 / clientWidth) * (max - min))
+
 		if (setting < min) setting = min
 		if (setting > max) setting = max
+
+		dispatch('input', { name, setting })
 	}
 
 	onDestroy(() => {
@@ -35,9 +67,16 @@
 	})
 </script>
 
-<div {name} class="range" {min} {max} on:change on:input bind:this={el} style:--thumb-width="{thumbWidth}px">
-	<div class="thumb" style:left="{progress}px" max="100" on:mousedown={mouseDown} />
-	<div class="progress" style:clip-path="0 {progress} 0 0" max="100" />
+<div
+	class="range"
+	{name}
+	bind:this={el}
+	style:--thumb-width="{thumbWidth}px"
+	draggable="false"
+	on:mousedown={setFromMouseX}
+>
+	<div class="thumb" bind:this={thumb} style:left="{progress}px" draggable="false" on:mousedown={mouseDown} />
+	<div class="track" bind:this={track} style:clip-path="0 {progress} 0 0" draggable="false" />
 </div>
 
 <style lang="scss">
@@ -56,7 +95,7 @@
 		}
 	}
 
-	.progress {
+	.track {
 		width: 100%;
 		height: 15px;
 
@@ -78,10 +117,9 @@
 
 		border: 1px solid var(--light-a);
 		border-radius: 20px;
-		background: var(--dark-d);
-		background-image: radial-gradient(ellipse at var(--progress) var(--progress), blue, red);
+		background: var(--light-d);
 
-		cursor: pointer;
+		cursor: grab;
 		transition: background 0.3s;
 		appearance: none;
 		appearance: none;
