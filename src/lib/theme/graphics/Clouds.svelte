270d8c2a2560d2516ef153931fe2f279dc50ce63
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-09-05 22:10:36 -0400

    [feat] better sun/stars

--- src/lib/theme/graphics/Clouds.svelte
+++ src/lib/theme/graphics/Clouds.svelte
@@ -1,23 +1,14 @@
 <script lang="ts">
-	import { fly } from 'svelte/transition';
+	import { fly } from 'svelte/transition'
 
-	export let size = 35;
+	export let size = 35
+	export let t_in = { x: -size, duration: 3000 }
+	export let t_out = { x: -size, duration: 6000, opacity: 1 }
 </script>
 
-<div
-	class="container"
-	style="width: {size}px;"
-	in:fly={{ x: -size, duration: 3000 }}
-	out:fly={{ x: -size, duration: 6000, opacity: 1 }}
->
-	<svg viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<circle
-			x="5"
-			y="5"
-			r="2.5"
-			fill="url(#CloudGradient1)"
-			filter="url(#CloudFilter)"
-		/>
+<div class="container" style="width: {size}px;" in:fly={t_in} out:fly={t_out}>
+	<svg viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg" overflow="visible">
+		<circle x="5" y="5" r="2.5" fill="url(#CloudGradient1)" filter="url(#CloudFilter)" />
 		<defs>
 			<filter
 				id="CloudFilter"
@@ -41,7 +32,7 @@
 						attributeName="baseFrequency"
 						values="0.05 0.1;0.1 0.5;0.05 0.1"
 						repeatCount="indefinite"
-						dur="240s"
+						dur="100s"
 					/>
 				</feTurbulence>
 				<feDisplacementMap
@@ -72,6 +63,7 @@
 	.container {
 		position: absolute;
 		z-index: 10;
+		inset: 0;
 	}
 	* {
 		backface-visibility: hidden;
