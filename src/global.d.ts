013f6c64982abe28097e16d425159e028962ae93
Braden Wiggins <55504972+FractalHQ@users.noreply.github.com>
2023-09-12 23:51:04 -0400

    [fix] add missing reference types

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,28 +1,11 @@
 /// <reference types="@sveltejs/kit" />
+/// <reference lib="dom.iterable" />
+/// <reference lib="vite/client" />
+/// <reference lib="node" />
+/// <reference lib="dom" />
 
 // *Action types
 // https://github.com/paolotiu/svelte-useactions/blob/main/src/index.ts
-
-/**
- * The return type of an action.
- */
-export type ActionReturn<Params> = {
-	destroy?: () => void
-	update?: (params: Params) => void
-} | void
-
-/**
- * Action type shim
- */
-export type ActionLike<Node extends HTMLElement> = (node: Node, params: any) => any
-
-/**
- * A primitive Action
- */
-export type Action<Node extends HTMLElement = HTMLElement, Params extends any = undefined> = (
-	node: Node,
-	params?: Params,
-) => ActionReturn<Params>
 
 declare global {
 	declare namespace svelte.JSX {
